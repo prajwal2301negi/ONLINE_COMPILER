ce1d3a13cfc27da4fefc328995f01c29a565c616
Prajwal Negi <166615651+prajwalnegi2301@users.noreply.github.com>
2024-07-16 23:08:04 +0530

    Add files via upload

---
+++ src/stubs.js
@@ -0,0 +1,18 @@
+const stubs = {};
+
+stubs.cpp = `#include<iostream>
+using namespace std;
+
+int main(){
+    
+    cout<<"hello world";
+
+    return 0;
+}
+`;
+
+stubs.py = `print("Hello world!")`;
+
+export default stubs;
+
+
